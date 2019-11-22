b272cb38528d5be635525fa421f1d064789c6034
Shane Geller <shanemgeller@gmail.com>
2019-11-22 15:37:43 -0500

    Update validators.py

--- srs/validators.py
+++ srs/validators.py
@@ -5,4 +5,4 @@ def validateImportFile(file):
 	ext = os.path.splitext(file.name)[1]
 	valid_extensions = ['.srsnote']
 	if not ext.lower() in valid_extensions:
-		raise ValidationError(u'Unsupported file extension. (.srsnote files only)')
+		raise ValidationError(u'Unsupported file extension (.srsnote files only).')
